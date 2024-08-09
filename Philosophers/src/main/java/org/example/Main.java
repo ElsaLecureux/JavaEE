01e928a47d7ab40fe47b38d79b657d162a6a05bb
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-08-09 10:30:22 +0200

    corrected how to manage forks for semaphore

--- Philosophers/src/main/java/org/example/Main.java
+++ Philosophers/src/main/java/org/example/Main.java
@@ -4,23 +4,22 @@ import java.util.concurrent.Semaphore;
 public class Main {
     static int number_of_philosophers = 5;
 
-    public static Semaphore[] forks = new Semaphore[number_of_philosophers];
+    //for Mutex idea of [] but not semaphore
+    //public static Semaphore[] forks = new Semaphore[number_of_philosophers];
+    public static Semaphore forks = new Semaphore(number_of_philosophers);
+
 
     public static Philosopher[] philosophers = new Philosopher[number_of_philosophers];
 
-    /*end program*/
-
-
     public static void main(String[] args) {
 
-        for ( int i = 0; i < number_of_philosophers ; i++ ) {
-            forks[i] = new Semaphore(1);
-        }
+        /*for Mutex for ( int i = 0; i < number_of_philosophers; i++ ){
+            create one fork for each philosopher
+        }*/
 
-        for ( int i = 0; i < number_of_philosophers ; i++ ) {
+        for ( int i = 0; i < number_of_philosophers; i++ ) {
             philosophers[i] = new Philosopher(i, 601, 200, 200, forks);
             new Thread(philosophers[i]).start();
         }
-
     }
 }
--- Philosophers/src/main/java/org/example/Philosopher.java
+++ Philosophers/src/main/java/org/example/Philosopher.java
@@ -9,20 +9,20 @@ public class Philosopher implements Runnable {
     int time_to_eat;
     int time_to_sleep;
 
-    Semaphore forkLeft;
+    Semaphore forks;
 
-    Semaphore forkRight;
+    // for Mutex not semaphore though
+    //Semaphore forkLeft;
+    //Semaphore forkRight;
 
     boolean isEquipped = false;
 
-    Philosopher(int number, int time_to_die, int time_to_eat, int time_to_sleep, Semaphore[] forks){
+    Philosopher(int number, int time_to_die, int time_to_eat, int time_to_sleep, Semaphore forks){
         this.number = number;
         this.time_to_die = time_to_die;
         this.time_to_eat = time_to_eat;
         this.time_to_sleep = time_to_sleep;
-
-        this.forkLeft = forks[number];
-        this.forkRight = forks[(number + 1)];
+        this.forks = forks;
     }
 
     public void eat(){
@@ -30,7 +30,7 @@ public class Philosopher implements Runnable {
         try {
             Thread.sleep(time_to_eat);
         } catch (InterruptedException e) {
-
+            Thread.currentThread().interrupt();
         }
     }
     public void sleep(){
@@ -38,28 +38,18 @@ public class Philosopher implements Runnable {
         try {
             Thread.sleep(time_to_sleep);
         } catch (InterruptedException e) {
-            System.out.println(e);
+            Thread.currentThread().interrupt();
         }
     }
     public void think(){
         System.out.println("Philosopher" + number + " is thinking");
     }
 
-    public boolean getTwoForks() {
-        /* trying to acquire 2 forks*/
-        if(forkRight.tryAcquire()) {
-            return forkLeft.tryAcquire();
-        } else {
-            //temporary
-            time_to_die = 0;
-            return false;
-        }
-    }
-
     public void run() {
         while (time_to_die > 0) {
-            if (getTwoForks()){
+            if (forks.tryAcquire(2)){
                 eat();
+                forks.release(2);
                 sleep();
             };
                 think();
