ba5d57bf23886e2f1cf0388685311c83df2c3f97
Elsa Lécureux <elsa.lecureux@gmail.com>
2024-08-09 12:13:08 +0200

    corrected grabbing forks

--- Philosophers/src/main/java/org/example/Main.java
+++ Philosophers/src/main/java/org/example/Main.java
@@ -6,19 +6,21 @@ public class Main {
 
     //for Mutex idea of [] but not semaphore
     //public static Semaphore[] forks = new Semaphore[number_of_philosophers];
-    public static Semaphore forks = new Semaphore(number_of_philosophers);
+
 
 
     public static Philosopher[] philosophers = new Philosopher[number_of_philosophers];
 
     public static void main(String[] args) {
 
+        Semaphore forks = new Semaphore(number_of_philosophers);
+
         /*for Mutex for ( int i = 0; i < number_of_philosophers; i++ ){
             create one fork for each philosopher
         }*/
 
         for ( int i = 0; i < number_of_philosophers; i++ ) {
-            philosophers[i] = new Philosopher(i, 601, 200, 200, forks);
+            philosophers[i] = new Philosopher(i + 1, 601, 200, 200, forks);
             new Thread(philosophers[i]).start();
         }
     }
--- Philosophers/src/main/java/org/example/Philosopher.java
+++ Philosophers/src/main/java/org/example/Philosopher.java
@@ -12,12 +12,12 @@ public class Philosopher implements Runnable {
     Semaphore forks;
 
     // for Mutex not semaphore though
-    //Semaphore forkLeft;
-    //Semaphore forkRight;
+    Semaphore forkLeft;
+    Semaphore forkRight;
 
     boolean isEquipped = false;
 
-    Philosopher(int number, int time_to_die, int time_to_eat, int time_to_sleep, Semaphore forks){
+    Philosopher(int number, int time_to_die, int time_to_eat, int time_to_sleep, Semaphore forks) {
         this.number = number;
         this.time_to_die = time_to_die;
         this.time_to_eat = time_to_eat;
@@ -25,35 +25,51 @@ public class Philosopher implements Runnable {
         this.forks = forks;
     }
 
-    public void eat(){
+    public void eat() {
         System.out.println("Philosopher" + number + " is eating");
         try {
             Thread.sleep(time_to_eat);
+            // change time_to_die
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
-    public void sleep(){
+
+    public void sleep() {
         System.out.println("Philosopher" + number + " is sleeping");
         try {
             Thread.sleep(time_to_sleep);
+            // change time_to_die - 200
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
-    public void think(){
+
+    public void think() {
         System.out.println("Philosopher" + number + " is thinking");
     }
 
     public void run() {
         while (time_to_die > 0) {
-            if (forks.tryAcquire(2)){
-                eat();
-                forks.release(2);
-                sleep();
-            };
-                think();
-                run();
+            try {
+               // System.out.println("Philosopher " + number + " is trying to acquire a fork");
+                forks.acquire(1);
+                try {
+                   // System.out.println("Philosopher " + number + " is trying to acquire second fork");
+                    forks.acquire(1);
+                   // System.out.println("Philosopher " + number + " has acquired 2 forks");
+                    eat();
+                   //System.out.println("Philosopher " + number + " is releasing 2 forks");
+                    forks.release(1);
+                    forks.release(1);
+                    sleep();
+                    think();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
